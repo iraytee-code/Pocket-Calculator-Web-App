d363f0efe7a2dad5dfa7dd72ca2c203b23eaaf68
iraytee-code <adeniranhop21@gmail.com>
2021-08-04 11:06:53 +0100

    themejs

---
+++ js/theme.js
@@ -0,0 +1,11 @@
+const themeBody = document.getElementById("checkbox");
+
+themeBody.addEventListener("change", (event) => {
+    document.body.classList.toggle("dark");
+});
+
+
+const themeScreen = document.getElementById("screen");
+
+
+
